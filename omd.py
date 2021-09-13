4e352ed55b153eb62eb5a1ad32b4f28766786741
innakysarat <82310490+innakysarat@users.noreply.github.com>
2021-09-13 23:53:07 +0300

    Add files via upload

--- omd.py
+++ omd.py
@@ -15,12 +15,20 @@ def step1():
 
 
 def step2_umbrella():
-    print('Бармен, принесите нам лучше пельменей\n'
-          'Рецепт простой: много мяса, мало теста')
+    print('\033[32mТогда утка с зонтиком просит пельменей\n'
+          'Рецепт простой: много мяса, мало теста\033[0m')
 
 
 def step2_no_umbrella():
-    print('Бармен, принесите-ка нам лучше 2 сидра')
+    print('\033[31mВы уверены, что хотите лишить утку зонтика?\033[0m')
+    option = ''
+    options = {'да': False, 'нет': True}
+    while option not in options:
+        print('Выберите: {}/{}'.format(*options))
+        option = input()
+    if options[option]:
+        return step2_umbrella()
+    print('\033[31mТогда утка без зонтика просит 2 сидра\033[0m')
 
 
 if __name__ == '__main__':
